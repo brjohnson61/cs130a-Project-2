99247a375c9afa72b97edc64f5488741041d56b6
Blake Johnson <brjohnson61@gmail.com>
2018-03-04 17:58:45 -0800

    added files

---
+++ Node.cpp
@@ -0,0 +1,91 @@
+//
+//  Node.cpp
+//  130a Project 1
+//
+//  Created by Blake Johnson on 1/31/18.
+//
+//
+
+#include "Node.hpp"
+#include <string>
+#include <iostream>
+#include <algorithm>
+#include<fstream>
+
+
+using namespace std;
+
+Node::Node(){
+    word = "";
+    count = 0;
+}
+
+Node::Node(string word){
+    transform(word.begin(), word.end(), word.begin(), ::tolower);
+    this->word = word;
+    count = 1;
+}
+
+Node::Node(Node* copy){
+    word = copy->getWord();
+    count = copy->getCount();
+}
+
+bool Node::operator==(Node& rhs){
+    if (this->word == rhs.getWord()){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+bool Node::operator>( Node& rhs){
+    if (this->word > rhs.getWord()){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+bool Node::operator<(Node& rhs){
+    if(this->word < rhs.getWord()){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+string Node::getWord(){
+    return this->word;
+}
+
+int Node::getCount(){
+    return this->count;
+}
+
+void Node::setWord(string word){
+    transform(word.begin(), word.end(), word.begin(), ::tolower);
+    this->word = word;
+}
+
+void Node::setCount(int count){
+    this->count = count;
+}
+
+void Node::increaseCount(){
+    this->count++;
+}
+
+void Node::decreaseCount(){
+    this->count--;
+}
+
+void outputToFile(string word, string pathName){
+    ofstream outputFile; 
+    outputFile.open(pathName, ios::app);
+    outputFile << word << endl;
+    outputFile.close();
+}
---
+++ Node.hpp
@@ -0,0 +1,44 @@
+
+//
+//  Node.hpp
+//  130a Project 1
+//
+//  Created by Blake Johnson on 1/31/18.
+//
+//
+
+#ifndef Node_hpp
+#define Node_hpp
+
+#include <stdio.h>
+#include <string>
+
+
+
+using namespace std;
+
+
+class Node {
+public:
+    Node();
+    Node(string word);
+    Node(Node* copy);
+    ~Node();
+    bool operator=(Node& rhs);
+    bool operator==( Node& rhs);
+    bool operator<( Node& rhs);
+    bool operator>( Node& rhs); 
+    string getWord(void);
+    int getCount(void);
+    void setWord(string word);
+    void setCount(int count);
+    void decreaseCount(void);
+    void increaseCount(void);
+private:
+    string word;
+    int count;
+};
+
+void outputToFile(string word, string pathName);
+
+#endif /* Node_hpp */
