40c4e8d0cd1aa0df52bbafe9833ea319ec4417d5
ferb120 <root@DESKTOP-26G29D7.localdomain>
2018-03-14 15:16:48 -0700

    Begain implementing BTreeNode

--- BTreeNode.cpp
+++ BTreeNode.cpp
@@ -4,28 +4,6 @@
 #include<stdio.h>
 #include"Node.hpp"
 
-using namespace std;
-
-BTreeNode::BTreeNode(){
-    keys = new Node[3];
-    children = new BTreeNode*[4];
-    numKeys = 0;
-    isLeaf = false;
-}
-
-
-int BTreeNode::search(Node node){
-    if (numKeys == 0){
-        return -1;
-    }
-    for (int i = 0; i< numKeys; i++){
-        if (keys[i] == node){
-            return i;
-        }else if (keys[i] > node){
-            return -1;
-        }
-    }
-}
 
 void BTreeNode::incrementNumKeys(){
     numKeys++;
